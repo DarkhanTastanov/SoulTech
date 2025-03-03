6d393ef57b19464c49b0c5f250a13971fbf05fc8
Darkhan Tastanov <tastanovd20@gmail.com>
2025-03-04 00:06:45 +0500

    some pages

--- app/src/main/java/com/example/myfaith/MainActivity.kt
+++ app/src/main/java/com/example/myfaith/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.myfaith
 
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -23,9 +25,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
-        val toolbar: Toolbar = findViewById(R.id.toolbar) // Get the toolbar
+
         val navController = findNavController(R.id.nav_host_fragment) // Get the NavController
 
         setSupportActionBar(toolbar) // Set up the toolbar
